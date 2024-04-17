3513f81afcba5684c2cc7ef47d018385dc781040
Thigo Dev <thiagomvk08@gmail.com>
2024-04-17 12:04:42 -0300

    build: adicionando dependências e iniciando o projeto

---
+++ src/main/java/br/com/api_neki/ApiNekiApplication.java
@@ -0,0 +1,13 @@
+package br.com.api_neki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNekiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api_neki/ApiNekiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_neki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNekiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
